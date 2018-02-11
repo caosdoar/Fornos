6f3d6aa695cc032ee0c7bf179b486118e37baafb
Oscar Sebio Cajaraville <mailoyo@gmail.com>
2018-02-11 16:56:23 +0100

    Better BVH construction
    Based on "On fast Construction of SAH-based Bounding Volume Hierarchies"
    Surface area cost is not implemented
    Over 25% performance improvement on AO times

--- Fornos/bvh.cpp
+++ Fornos/bvh.cpp
@@ -1,62 +1,56 @@
 #include "bvh.h"
 #include "mesh.h"
+#include <cassert>
 
-void binaryDivisionBVH(const Mesh *mesh, const size_t maxTriangleCount, const size_t maxTreeDepth, BVH &parent, const size_t currentDepth)
+inline size_t bestSplitFromBuckets(const uint32_t buckets[16], const uint32_t tricount)
 {
-	if (parent.triangles.size() <= maxTriangleCount ||
-		currentDepth >= maxTreeDepth)
+	int l = 0;
+	int r = (int)tricount;
+	int diff = INT_MAX;
+	size_t best = 0;
+	for (size_t i = 0; i < 16; ++i)
 	{
-		return;
-	}
-
-	Vector3 offset;
-
-	const Vector3 parentSize = parent.aabb.size;
-	if (parentSize.x >= parentSize.y)
-	{
-		if (parentSize.x >= parentSize.z)
+		const uint32_t c = buckets[i];
+		l += c;
+		r -= c;
+		int d = abs(l - r);
+		if (d > diff)
 		{
-			offset = Vector3(parentSize.x * 0.5f, 0.0f, 0.0f);
+			break;
 		}
-		else
-		{
-			offset = Vector3(0.0f, 0.0f, parentSize.z * 0.5f);
-		}
+		diff = d;
+		best = i;
 	}
-	else if (parentSize.y >= parentSize.z)
-	{
-		offset = Vector3(0.0f, parentSize.y * 0.5f, 0.0f);
-	}
-	else
-	{
-		offset = Vector3(0.0f, 0.0f, parentSize.z * 0.5f);
-	}
+	return best;
+}
 
-	const Vector3 size = parent.aabb.size - offset;
-	const AABB left(parent.aabb.center - offset, size);
-	const AABB right(parent.aabb.center + offset, size);
-
-	parent.children.resize(2);
-	parent.children[0].aabb = left;
-	parent.children[1].aabb = right;
-
+template <typename Axis>
+float findSplit(const Mesh *mesh, const BVH &parent)
+{
+	const float minX = Axis::get(parent.aabb.center) - Axis::get(parent.aabb.size);
+	const float maxX = Axis::get(parent.aabb.center) + Axis::get(parent.aabb.size);
+	uint32_t buckets[16] = { 0 };
 	for (uint32_t tidx : parent.triangles)
 	{
 		const Mesh::Triangle &tri = mesh->triangles[tidx];
 		const Vector3 p0 = mesh->positions[mesh->vertices[tri.vertexIndex0].positionIndex];
 		const Vector3 p1 = mesh->positions[mesh->vertices[tri.vertexIndex1].positionIndex];
 		const Vector3 p2 = mesh->positions[mesh->vertices[tri.vertexIndex2].positionIndex];
-		const Triangle t(p0, p1, p2);
-		if (TriangleAABB(t, left)) parent.children[0].triangles.push_back(tidx);
-		if (TriangleAABB(t, right)) parent.children[1].triangles.push_back(tidx);
+		const Vector3 centroid = (p0 + p1 + p2) / 3.0f;
+		const size_t i = size_t((Axis::get(centroid) - minX) / (maxX - minX) * 15.99f);
+		assert(i < 16);
+		++buckets[i];
 	}
 
-	parent.triangles.clear();
-	binaryDivisionBVH(mesh, maxTriangleCount, maxTreeDepth, parent.children[0], currentDepth+1);
-	binaryDivisionBVH(mesh, maxTriangleCount, maxTreeDepth, parent.children[1], currentDepth+1);
+	size_t best = bestSplitFromBuckets(buckets, uint32_t(parent.triangles.size()));
+	return minX + (maxX - minX) / 16.0f * float(best + 1);
 }
 
-void binaryDivisionBVH_v2(const Mesh *mesh, const size_t maxTriangleCount, const size_t maxTreeDepth, BVH &parent, const size_t currentDepth)
+struct AxisX { static float get(const Vector3 &v) { return v.x; } };
+struct AxisY { static float get(const Vector3 &v) { return v.y; } };
+struct AxisZ { static float get(const Vector3 &v) { return v.z; } };
+
+void binaryDivisionBVH(const Mesh *mesh, const size_t maxTriangleCount, const size_t maxTreeDepth, BVH &parent, const size_t currentDepth)
 {
 	if (parent.triangles.size() <= maxTriangleCount ||
 		currentDepth >= maxTreeDepth)
@@ -67,74 +61,107 @@ void binaryDivisionBVH_v2(const Mesh *mesh, const size_t maxTriangleCount, const
 
 	parent.children.resize(2);
 
+	Vector3 aabbMinL(FLT_MAX); Vector3 aabbMaxL(-FLT_MAX);
+	Vector3 aabbMinR(FLT_MAX); Vector3 aabbMaxR(-FLT_MAX);
+
 	const Vector3 parentSize = parent.aabb.size;
 	if (parentSize.x >= parentSize.y && parentSize.x >= parentSize.z)
 	{
 		// Divide X
-		const float xTest = parent.aabb.center.x;
+		//const float xTest = findSplitX(mesh, parent);
+		const float xTest = findSplit<AxisX>(mesh, parent);
 		for (uint32_t tidx : parent.triangles)
 		{
 			const Mesh::Triangle &tri = mesh->triangles[tidx];
 			const Vector3 p0 = mesh->positions[mesh->vertices[tri.vertexIndex0].positionIndex];
 			const Vector3 p1 = mesh->positions[mesh->vertices[tri.vertexIndex1].positionIndex];
 			const Vector3 p2 = mesh->positions[mesh->vertices[tri.vertexIndex2].positionIndex];
-			const bool left =  p0.x <= xTest || p1.x <= xTest || p2.x <= xTest;
-			const bool right = p0.x >= xTest || p1.x >= xTest || p2.x >= xTest;
-			if (left)  parent.children[0].triangles.push_back(tidx);
-			if (right) parent.children[1].triangles.push_back(tidx);
+			const Vector3 c = (p0 + p1 + p2) * (1.0f / 3.0f);
+			if (c.x <= xTest)
+			{
+				parent.children[0].triangles.push_back(tidx);
+				aabbMinL = min(aabbMinL, min(p0, min(p1, p2)));
+				aabbMaxL = max(aabbMaxL, max(p0, max(p1, p2)));
+			}
+			else
+			{
+				parent.children[1].triangles.push_back(tidx);
+				aabbMinR = min(aabbMinR, min(p0, min(p1, p2)));
+				aabbMaxR = max(aabbMaxR, max(p0, max(p1, p2)));
+			}
 		}
-
-		const Vector3 offset(parentSize.x * 0.5f, 0.0f, 0.0f);
-		const Vector3 size = parent.aabb.size - offset;
-		parent.children[0].aabb = AABB(parent.aabb.center - offset, size);
-		parent.children[1].aabb = AABB(parent.aabb.center + offset, size);
 	}
 	else if (parentSize.y >= parentSize.z)
 	{
 		// Divide Y
-		const float yTest = parent.aabb.center.y;
+		const float yTest = findSplit<AxisY>(mesh, parent);
 		for (uint32_t tidx : parent.triangles)
 		{
 			const Mesh::Triangle &tri = mesh->triangles[tidx];
 			const Vector3 p0 = mesh->positions[mesh->vertices[tri.vertexIndex0].positionIndex];
 			const Vector3 p1 = mesh->positions[mesh->vertices[tri.vertexIndex1].positionIndex];
 			const Vector3 p2 = mesh->positions[mesh->vertices[tri.vertexIndex2].positionIndex];
-			const bool left =  p0.y <= yTest || p1.y <= yTest || p2.y <= yTest;
-			const bool right = p0.y >= yTest || p1.y >= yTest || p2.y >= yTest;
-			if (left)  parent.children[0].triangles.push_back(tidx);
-			if (right) parent.children[1].triangles.push_back(tidx);
+			const Vector3 c = (p0 + p1 + p2) * (1.0f / 3.0f);
+			if (c.y <= yTest)
+			{
+				parent.children[0].triangles.push_back(tidx);
+				aabbMinL = min(aabbMinL, min(p0, min(p1, p2)));
+				aabbMaxL = max(aabbMaxL, max(p0, max(p1, p2)));
+			}
+			else
+			{
+				parent.children[1].triangles.push_back(tidx);
+				aabbMinR = min(aabbMinR, min(p0, min(p1, p2)));
+				aabbMaxR = max(aabbMaxR, max(p0, max(p1, p2)));
+			}
 		}
 
-		const Vector3 offset(0.0f, parentSize.y * 0.5f, 0.0f);
-		const Vector3 size = parent.aabb.size - offset;
-		parent.children[0].aabb = AABB(parent.aabb.center - offset, size);
-		parent.children[1].aabb = AABB(parent.aabb.center + offset, size);
 	}
 	else
 	{
 		// Divide Z
-		const float zTest = parent.aabb.center.z;
+		const float zTest = findSplit<AxisZ>(mesh, parent);
 		for (uint32_t tidx : parent.triangles)
 		{
 			const Mesh::Triangle &tri = mesh->triangles[tidx];
 			const Vector3 p0 = mesh->positions[mesh->vertices[tri.vertexIndex0].positionIndex];
 			const Vector3 p1 = mesh->positions[mesh->vertices[tri.vertexIndex1].positionIndex];
 			const Vector3 p2 = mesh->positions[mesh->vertices[tri.vertexIndex2].positionIndex];
-			const bool left =  p0.z <= zTest || p1.z <= zTest || p2.z <= zTest;
-			const bool right = p0.z >= zTest || p1.z >= zTest || p2.z >= zTest;
-			if (left)  parent.children[0].triangles.push_back(tidx);
-			if (right) parent.children[1].triangles.push_back(tidx);
+			const Vector3 c = (p0 + p1 + p2) * (1.0f / 3.0f);
+			if (c.z <= zTest)
+			{
+				parent.children[0].triangles.push_back(tidx);
+				aabbMinL = min(aabbMinL, min(p0, min(p1, p2)));
+				aabbMaxL = max(aabbMaxL, max(p0, max(p1, p2)));
+			}
+			else
+			{
+				parent.children[1].triangles.push_back(tidx);
+				aabbMinR = min(aabbMinR, min(p0, min(p1, p2)));
+				aabbMaxR = max(aabbMaxR, max(p0, max(p1, p2)));
+			}
 		}
-
-		const Vector3 offset(0.0f, 0.0f, parentSize.z * 0.5f);
-		const Vector3 size = parent.aabb.size - offset;
-		parent.children[0].aabb = AABB(parent.aabb.center - offset, size);
-		parent.children[1].aabb = AABB(parent.aabb.center + offset, size);
 	}
 
 	parent.triangles.clear();
-	binaryDivisionBVH_v2(mesh, maxTriangleCount, maxTreeDepth, parent.children[0], currentDepth + 1);
-	binaryDivisionBVH_v2(mesh, maxTriangleCount, maxTreeDepth, parent.children[1], currentDepth + 1);
+
+	parent.children[0].aabb = AABB((aabbMinL + aabbMaxL) * 0.5f, (aabbMaxL - aabbMinL) * 0.5f);
+	parent.children[1].aabb = AABB((aabbMinR + aabbMaxR) * 0.5f, (aabbMaxR - aabbMinR) * 0.5f);
+
+	binaryDivisionBVH(mesh, maxTriangleCount, maxTreeDepth, parent.children[0], currentDepth + 1);
+	binaryDivisionBVH(mesh, maxTriangleCount, maxTreeDepth, parent.children[1], currentDepth + 1);
+
+	// Update AABB
+	{
+		aabbMinL = parent.children[0].aabb.center - parent.children[0].aabb.size;
+		aabbMaxL = parent.children[0].aabb.center + parent.children[0].aabb.size;
+		aabbMinR = parent.children[1].aabb.center - parent.children[1].aabb.size;
+		aabbMaxR = parent.children[1].aabb.center + parent.children[1].aabb.size;
+		const Vector3 aabbMin = min(aabbMinL, aabbMinR);
+		const Vector3 aabbMax = max(aabbMaxL, aabbMaxR);
+		parent.aabb.center = (aabbMax + aabbMin) * 0.5f;
+		parent.aabb.size = (aabbMax - aabbMin) * 0.5f;
+	}
 
 	parent.subtreeTriangleCount =
 		parent.children[0].subtreeTriangleCount +
@@ -163,7 +190,7 @@ BVH* BVH::createBinary(const Mesh *mesh, const size_t maxTriangleCount, const si
 		bvh->triangles[i] = (uint32_t)i;
 	}
 
-	binaryDivisionBVH_v2(mesh, maxTriangleCount, maxTreeDepth, *bvh, 0);
+	binaryDivisionBVH(mesh, maxTriangleCount, maxTreeDepth, *bvh, 0);
 
 	return bvh;
 }
