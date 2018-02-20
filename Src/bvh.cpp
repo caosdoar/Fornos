5eed10e8fc6520043d5af1d284cf0f9bfb80fbac
Oscar Sebio Cajaraville <mailoyo@gmail.com>
2018-02-20 12:42:15 +0100

    BVH generation improvements
    Fully implemented the paper "On fast Construction of SAH-based Bounding Volume Hierarchies".
    
    Our code checks all three axis for each subdivision but the least cost axis is not always the fastests for our raycast. In one of our two test meshes using the least cost axis pro is about 40% slower when calculating AO. But in the other mesh using the largest axis is 400% slower. So sticking to the least cost axis for now. Need more test meshes though.

--- Src/bvh.cpp
+++ Src/bvh.cpp
@@ -26,9 +26,10 @@ SOFTWARE.
 #include "timing.h"
 #include <cassert>
 
-struct Split
+enum class Axis { X, Y, Z };
+
+struct SplitResult
 {
-	enum class Axis { X, Y, Z };
 	Axis axis;
 	float split;
 };
@@ -36,11 +37,39 @@ struct Split
 struct BucketSplit
 {
 	size_t splitIdx;
-	float quality;
+	float cost;
 };
 
-inline BucketSplit selectSplitFromBuckets(const uint32_t buckets[16], const uint32_t tricount)
+struct BucketAABB
 {
+	Vector3 minv = Vector3(FLT_MAX);
+	Vector3 maxv = Vector3(-FLT_MAX);
+
+	void addPoint(const Vector3 &p)
+	{
+		minv = min(p, minv);
+		maxv = max(p, maxv);
+	}
+};
+
+BucketAABB combine(const BucketAABB &aabb0, const BucketAABB &aabb1)
+{
+	BucketAABB r = aabb0;
+	r.minv = min(aabb1.minv, r.minv);
+	r.maxv = max(aabb1.maxv, r.maxv);
+	return r;
+}
+
+inline float computeCost(const BucketAABB &aabb, const int triangleCount)
+{
+	const Vector3 size = aabb.maxv - aabb.minv;
+	const float surfaceArea = 2.0f * (size.x * size.y + size.x * size.z + size.x * size.y);
+	return float(triangleCount) * surfaceArea;
+}
+
+inline BucketSplit selectSplitFromBuckets(const uint32_t buckets[16], const BucketAABB bucketsAABB[16], size_t triangleCount)
+{
+#if 0
 	int l = 0;
 	int r = (int)tricount;
 	int diff = INT_MAX;
@@ -60,14 +89,47 @@ inline BucketSplit selectSplitFromBuckets(const uint32_t buckets[16], const uint
 	}
 	const float quality = 1.0f - float(diff) / float(tricount);
 	return BucketSplit{ best, quality };
+#else
+	// Pass to compute AABB on the right side of the split
+	BucketAABB aabbRightSide[15];
+	aabbRightSide[14] = bucketsAABB[15];
+	for (int i = 13; i >= 0; --i)
+	{
+		aabbRightSide[i] = combine(aabbRightSide[i + 1], bucketsAABB[i + 1]);
+	}
+
+	float bestCost = FLT_MAX;
+	int bestSplitIdx = 0;
+
+	BucketAABB aabbLeft;
+	int countLeft = 0;
+	int countRight = int(triangleCount);
+	for (int i = 0; i < 16; ++i)
+	{
+		const auto c = buckets[i];
+		countLeft += c;
+		countRight -= c;
+		aabbLeft = combine(aabbLeft, bucketsAABB[i]);
+		BucketAABB aabbRight = aabbRightSide[i];
+		const float costLeft = computeCost(aabbLeft, countLeft);
+		const float costRight = computeCost(aabbRight, countRight);
+		const float cost = costLeft + costRight;
+		if (cost < bestCost)
+		{
+			bestCost = cost;
+			bestSplitIdx = i;
+		}
+	}
+
+	return BucketSplit{ size_t(bestSplitIdx), bestCost };
+#endif
 }
 
-Split findBestSplit(const Mesh *mesh, const BVH &parent)
+SplitResult findBestSplit(const Mesh *mesh, const BVH &parent)
 {
-	Split ret;
+	SplitResult ret;
 
-	Vector3 mins = Vector3(FLT_MAX);
-	Vector3 maxs = Vector3(-FLT_MAX);
+	BucketAABB centroiddsAABB;
 	for (uint32_t tidx : parent.triangles)
 	{
 		const Mesh::Triangle &tri = mesh->triangles[tidx];
@@ -75,13 +137,16 @@ Split findBestSplit(const Mesh *mesh, const BVH &parent)
 		const Vector3 p1 = mesh->positions[mesh->vertices[tri.vertexIndex1].positionIndex];
 		const Vector3 p2 = mesh->positions[mesh->vertices[tri.vertexIndex2].positionIndex];
 		const Vector3 centroid = (p0 + p1 + p2) / 3.0f;
-		mins = min(mins, centroid);
-		maxs = max(maxs, centroid);
+		centroiddsAABB.addPoint(centroid);
 	}
 
 	uint32_t bucketsX[16] = { 0 };
 	uint32_t bucketsY[16] = { 0 };
 	uint32_t bucketsZ[16] = { 0 };
+	BucketAABB bucketsAABBX[16];
+	BucketAABB bucketsAABBY[16];
+	BucketAABB bucketsAABBZ[16];
+
 	for (uint32_t tidx : parent.triangles)
 	{
 		const Mesh::Triangle &tri = mesh->triangles[tidx];
@@ -89,35 +154,49 @@ Split findBestSplit(const Mesh *mesh, const BVH &parent)
 		const Vector3 p1 = mesh->positions[mesh->vertices[tri.vertexIndex1].positionIndex];
 		const Vector3 p2 = mesh->positions[mesh->vertices[tri.vertexIndex2].positionIndex];
 		const Vector3 centroid = (p0 + p1 + p2) / 3.0f;
-		const Vector3 ijk = (centroid - mins) / (maxs - mins) * 15.99f;
+
+		const Vector3 ijk = (centroid - centroiddsAABB.minv) / (centroiddsAABB.maxv - centroiddsAABB.minv) * 15.99f;
 		const size_t i = isnan(ijk.x) ? 0 : size_t(ijk.x);
 		const size_t j = isnan(ijk.y) ? 0 : size_t(ijk.y);
 		const size_t k = isnan(ijk.z) ? 0 : size_t(ijk.z);
 		assert(i < 16 && j < 16 && k < 16);
+
 		++bucketsX[i];
 		++bucketsY[j];
 		++bucketsZ[k];
+
+		bucketsAABBX[i].addPoint(p0);
+		bucketsAABBX[i].addPoint(p1);
+		bucketsAABBX[i].addPoint(p2);
+		bucketsAABBY[j].addPoint(p0);
+		bucketsAABBY[j].addPoint(p1);
+		bucketsAABBY[j].addPoint(p2);
+		bucketsAABBZ[k].addPoint(p0);
+		bucketsAABBZ[k].addPoint(p1);
+		bucketsAABBZ[k].addPoint(p2);
 	}
 
 	const uint32_t tricount = uint32_t(parent.triangles.size());
-	const BucketSplit splitX = selectSplitFromBuckets(bucketsX, tricount);
-	const BucketSplit splitY = selectSplitFromBuckets(bucketsY, tricount);
-	const BucketSplit splitZ = selectSplitFromBuckets(bucketsZ, tricount);
+	const BucketSplit splitX = selectSplitFromBuckets(bucketsX, bucketsAABBX, tricount);
+	const BucketSplit splitY = selectSplitFromBuckets(bucketsY, bucketsAABBY, tricount);
+	const BucketSplit splitZ = selectSplitFromBuckets(bucketsZ, bucketsAABBZ, tricount);
 
-	if (splitX.quality >= splitY.quality && splitX.quality >= splitZ.quality)
+	if (splitX.cost <= splitY.cost && splitX.cost <= splitZ.cost)
+	//if (parent.aabb.size.x >= parent.aabb.size.y && parent.aabb.size.x >= parent.aabb.size.z)
 	{
-		ret.axis = Split::Axis::X;
-		ret.split = mins.x + (maxs.x - mins.x) / 16.0f * float(splitX.splitIdx + 1);
+		ret.axis = Axis::X;
+		ret.split = centroiddsAABB.minv.x + (centroiddsAABB.maxv.x - centroiddsAABB.minv.x) / 16.0f * float(splitX.splitIdx + 1);
 	}
-	else if (splitY.quality >= splitX.quality && splitY.quality >= splitZ.quality)
+	else if (splitY.cost <= splitX.cost && splitY.cost <= splitZ.cost)
+	//else if (parent.aabb.size.y >= parent.aabb.size.x && parent.aabb.size.y >= parent.aabb.size.z)
 	{
-		ret.axis = Split::Axis::Y;
-		ret.split = mins.y + (maxs.y - mins.y) / 16.0f * float(splitY.splitIdx + 1);
+		ret.axis = Axis::Y;
+		ret.split = centroiddsAABB.minv.y + (centroiddsAABB.maxv.y - centroiddsAABB.minv.y) / 16.0f * float(splitY.splitIdx + 1);
 	}
 	else
 	{
-		ret.axis = Split::Axis::Z;
-		ret.split = mins.z + (maxs.z - mins.z) / 16.0f * float(splitZ.splitIdx + 1);
+		ret.axis = Axis::Z;
+		ret.split = centroiddsAABB.minv.z + (centroiddsAABB.maxv.z - centroiddsAABB.minv.z) / 16.0f * float(splitZ.splitIdx + 1);
 	}
 
 	return ret;
@@ -137,7 +216,7 @@ void binaryDivisionBVH(const Mesh *mesh, const size_t maxTriangleCount, const si
 	Vector3 aabbMinL(FLT_MAX); Vector3 aabbMaxL(-FLT_MAX);
 	Vector3 aabbMinR(FLT_MAX); Vector3 aabbMaxR(-FLT_MAX);
 
-	const Split split = findBestSplit(mesh, parent);
+	const SplitResult split = findBestSplit(mesh, parent);
 
 	for (uint32_t tidx : parent.triangles)
 	{
@@ -148,9 +227,9 @@ void binaryDivisionBVH(const Mesh *mesh, const size_t maxTriangleCount, const si
 		const Vector3 c = (p0 + p1 + p2) * (1.0f / 3.0f);
 
 		const bool left =
-			((split.axis == Split::Axis::X) & (c.x <= split.split)) |
-			((split.axis == Split::Axis::Y) & (c.y <= split.split)) |
-			((split.axis == Split::Axis::Z) & (c.z <= split.split));
+			((split.axis == Axis::X) & (c.x <= split.split)) |
+			((split.axis == Axis::Y) & (c.y <= split.split)) |
+			((split.axis == Axis::Z) & (c.z <= split.split));
 
 		if (left)
 		{
